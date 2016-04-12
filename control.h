c3862cd318b665df87dbe7d391230c720c69acce
dawufenfen <674217739@qq.com>
2016-04-12 20:59:54 +0800

    Create control.h

---
+++ control.h
@@ -0,0 +1,49 @@
+// control.h : main header file for the control application
+//
+
+#if !defined(AFX__H__56D17D77_EA2B_4F03_9630_9E216D2C7B63__INCLUDED_)
+#define AFX__H__56D17D77_EA2B_4F03_9630_9E216D2C7B63__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#ifndef __AFXWIN_H__
+	#error include 'stdafx.h' before including this file for PCH
+#endif
+
+#include "resource.h"		// main symbols
+
+/////////////////////////////////////////////////////////////////////////////
+// CMyApp:
+// See main.cpp for the implementation of this class
+//
+
+class CMyApp : public CWinApp
+{
+public:
+	CMyApp();
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CMyApp)
+	public:
+	virtual BOOL InitInstance();
+	//}}AFX_VIRTUAL
+
+// Implementation
+
+	//{{AFX_MSG(CMyApp)
+		// NOTE - the ClassWizard will add and remove member functions here.
+		//    DO NOT EDIT what you see in these blocks of generated code !
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX__H__56D17D77_EA2B_4F03_9630_9E216D2C7B63__INCLUDED_)
